5ebd9644f2c5cbe76774d43f77f95ae74c9fc51c
NAHID12105080 <nahid151341@gmail.com>
2024-12-30 11:49:46 +0600

    fixed the bug in levelwise traversal with endline

--- tree/concept-building/1.Binary_tree_formation.cpp
+++ tree/concept-building/1.Binary_tree_formation.cpp
@@ -87,6 +87,7 @@ void LevelOrderWithSeperateLine(Node* root){
         if(q.empty()){
             break;
         }
+        q.push(NULL);
 
         }
 
@@ -100,6 +101,9 @@ void LevelOrderWithSeperateLine(Node* root){
         }
     }
 }
+
+//find height of tree
+
 
 signed main() {
    vector<int> nodes={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
