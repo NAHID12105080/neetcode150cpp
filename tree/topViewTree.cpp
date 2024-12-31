11cea9c8aa0b319cbae7ddc434b1cb6e585be10d
NAHID12105080 <nahid151341@gmail.com>
2024-12-31 19:37:52 +0600

    logic to find lowestCommonAncestor

--- tree/topViewTree.cpp
+++ tree/topViewTree.cpp
@@ -84,7 +84,7 @@ void printKthLevelNodes(Node* root, int k) {
     kthNodePrintHelper(root, k, 1);
     cout << endl;
 }
-//2nd approach
+//2nd approach, trust recursion
 void KthLevelNodes(Node* root, int k) {
     if (root == NULL) {
         return;
@@ -97,7 +97,36 @@ void KthLevelNodes(Node* root, int k) {
     KthLevelNodes(root->right, k - 1);
 }
 
+bool findPath(Node* root, int n, vector<int>& path) {
+    if (root == NULL) {
+        return false;
+    }
+    path.push_back(root->data);
+    if (root->data == n) {
+        return true;
+    }
+    if (findPath(root->left, n, path) || findPath(root->right, n, path)) {
+        return true;
+    }
+    path.pop_back();
+    return false;
+}
 
+int LowestCommonAncestor(Node* root, int n1, int n2) {
+    vector<int> path1, path2;
+    if (!findPath(root, n1, path1) || !findPath(root, n2, path2)) {
+        cout << "No path found";
+        return -1;
+    }
+    int lca = -1;
+    for (int i = 0, j = 0; i < path1.size() && j < path2.size(); i++, j++) {
+        if (path1[i] != path2[j]) {
+            break;
+        }
+        lca = path1[i];
+    }
+    return lca;
+}
 
 signed main() {
 vector<int> nodes = {1, 2, 4, 7, -1, -1, 8, -1, -1, 5, -1, 9, -1, -1, 3, -1, 6, 10, -1, -1, 11, -1, -1};
@@ -107,7 +136,9 @@ vector<int> nodes = {1, 2, 4, 7, -1, -1, 8, -1, -1, 5, -1, 9, -1, -1, 3, -1, 6, 
     // levelOrder(root);
     // cout<<endl;
     // Topview(root);
-    printKthLevelNodes(root,4);
-    KthLevelNodes(root,4);
+    // printKthLevelNodes(root,4);
+    // KthLevelNodes(root,4);
+
+    cout<<LowestCommonAncestor(root,10,11);
   return 0;
 }
