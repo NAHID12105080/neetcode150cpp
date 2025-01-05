5fa2d07743c3daa5c52daa4a9c6f65be757db849
NAHID12105080 <nahid151341@gmail.com>
2025-01-05 16:27:15 +0600

    added logical code delete a node from a bst

--- BST/code/1.Bst_basics.cpp
+++ BST/code/1.Bst_basics.cpp
@@ -45,12 +45,75 @@ void inorder(Node* root){
     inorder(root->right);
 }
 
+bool searchBST(Node* root, int key){
+    if(root==NULL){
+        return false;
+    }
+    if(root->data==key){
+        return true;
+    }
+    if(key<root->data){
+        return searchBST(root->left,key);
+    }
+    return searchBST(root->right,key);
+    
+}
+
+
+Node* InorderSuccessor(Node* root){
+    while(root->left!=NULL){
+        root=root->left;
+    }
+
+    return root;
+}
+
+
+Node* deleteNode(Node* root, int val){
+    if(root==NULL) {
+        return NULL;
+    }
+    if(val<root->data){
+         root->left=deleteNode(root->left,val);
+    }
+    if(val>root->data){
+         root->right=deleteNode(root->right,val);
+    }
+    else{
+        if(root->left==NULL&& root->right==NULL){
+            delete root;
+            return NULL;
+        }
+
+        if(root->left==NULL || root->right==NULL){
+            return root->left==NULL?root->right:root->left;
+        }
+
+        //2child case:
+        else{
+            Node* IS=InorderSuccessor(root->right);
+            root->data=IS->data;
+            root->right=deleteNode(root->right,IS->data);
+            return root;
+        }
+    }
+    return root;
+}
 
 
 int main(){
     int arr[]={11,1,2,3,4,5};
     Node* root=buildBST(arr,6);
+    // inorder(root);
+    // cout<<endl;
+    // cout<<searchBST(root,5)<<endl;
+
+
     inorder(root);
+    deleteNode(root,5);
+    cout<<endl;
+    inorder(root);
+
 
     return 0;
 }
