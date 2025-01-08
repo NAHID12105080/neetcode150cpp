8254e72cebd607757c196c12bf2ff0e5f1832a39
NAHID12105080 <nahid151341@gmail.com>
2025-01-08 16:05:33 +0600

    pq done with heap class

--- Heaps/heap_pq_implementation.cpp
+++ Heaps/heap_pq_implementation.cpp
@@ -21,8 +21,30 @@ class Heap{
         }
     }
 
-    void pop(){
 
+
+    void pop(){
+        int n=v.size();
+        swap(v[0],v[n-1]);
+        v.pop_back();
+        heapify(0);
+    }
+    void heapify(int i){
+        int n = v.size();
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && v[left] > v[largest])
+            largest = left;
+
+        if (right < n && v[right] > v[largest])
+            largest = right;
+
+        if (largest != i) {
+            swap(v[i], v[largest]);
+            heapify(largest);
+        }
     }
 
     int top(){
