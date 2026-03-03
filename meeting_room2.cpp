5fcf1a85f8a93290c0bfc831b7952fd4e80ba5d8
Md Nahid Hassan Bhuiyan <nahid151341@gmail.com>
2026-03-03 13:21:56 +0600

    meeting room2 solved using line swap from gfg

---
+++ meeting_room2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    int minMeetingRooms(vector<int> &start, vector<int> &end) {
+        // code here
+        map<int,int>mp;
+        
+        int n=start.size();
+        
+        for(int i=0;i<n;i++){
+            mp[start[i]]++;
+            mp[end[i]]--;
+            
+        }
+            int overlap=0;
+            int maxValue=0;
+        for(auto &[u,v]:mp){
+            overlap+=v;
+            
+            if(overlap>maxValue){
+                maxValue=overlap;
+            }
+        }
+        return maxValue;
+    }
+};
