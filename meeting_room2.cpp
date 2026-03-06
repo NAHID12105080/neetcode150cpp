be5cd555929eed2449052c2fe4a74eae849aa57d
Md Nahid Hassan Bhuiyan <nahid151341@gmail.com>
2026-03-06 22:27:17 +0600

    monotonic queue explored and leetcode 209 solved

--- meeting_room2.cpp
+++ meeting_room2.cpp
@@ -1,25 +1,27 @@
-class Solution {
-  public:
-    int minMeetingRooms(vector<int> &start, vector<int> &end) {
-        // code here
-        map<int,int>mp;
+
+
+// class Solution {
+//   public:
+//     int minMeetingRooms(vector<int> &start, vector<int> &end) {
+//         // code here
+//         map<int,int>mp;
         
-        int n=start.size();
+//         int n=start.size();
         
-        for(int i=0;i<n;i++){
-            mp[start[i]]++;
-            mp[end[i]]--;
+//         for(int i=0;i<n;i++){
+//             mp[start[i]]++;
+//             mp[end[i]]--;
             
-        }
-            int overlap=0;
-            int maxValue=0;
-        for(auto &[u,v]:mp){
-            overlap+=v;
+//         }
+//             int overlap=0;
+//             int maxValue=0;
+//         for(auto &[u,v]:mp){
+//             overlap+=v;
             
-            if(overlap>maxValue){
-                maxValue=overlap;
-            }
-        }
-        return maxValue;
-    }
-};
+//             if(overlap>maxValue){
+//                 maxValue=overlap;
+//             }
+//         }
+//         return maxValue;
+//     }
+// };
